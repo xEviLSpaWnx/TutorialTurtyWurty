4addb78a28dc0527fbbea0015ae6c684f823e4a3
Darren Howley <d.howley77@gmail.com>
2020-12-19 01:05:59 +0000

    Initial Upload.

---
+++ src/main/java/com/evilspawn/tutorialmod/TutorialMod.java
@@ -0,0 +1,25 @@
+package com.evilspawn.tutorialmod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("tutorialmod")
+public class TutorialMod
+{
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public TutorialMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) { }
+    private void doClientStuff(final FMLClientSetupEvent event) { }
+}
